5ec93efbac311f69a92c14c38996e525ec7412e2
pjohans <pjohans@gmail.com>
2012-10-31 07:17:08 +0100

    action links on favourite

--- bibdk_favourite_library.tpl.php
+++ bibdk_favourite_library.tpl.php
@@ -3,13 +3,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+//dpm($variables);
 ?>
 
 <div class="element-section padded">
   <hgroup>
     <h3><?php print $branchName; ?></h3>      
-  </hgroup>
-  <div class="actions">
+  </hgroup> 
+  <div class="actions">   
     <?php print $actions; ?>
   </div>
   <div class="toggle-next-section">
