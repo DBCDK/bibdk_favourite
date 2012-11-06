49d63cd539def05e14555a43443e69f64ee660ed
pjohans <pjohans@gmail.com>
2012-11-06 11:12:55 +0100

    set class on favourite

---
+++ js/setFavourite.js
@@ -0,0 +1,21 @@
+/* 
+ * add this function to Drupal.ajax.prototype commands 
+ * to use it as standard drupal #ajax
+ */
+
+(function ($){
+    // add this function to Drupal ajax commands
+    Drupal.ajax.prototype.commands.favourite_set = function(ajax, response, status){
+        // remove order-agency class for all articles
+        $('article').removeClass('order-agency');
+        // set given artice as selected
+        var selector = response['selector'];
+        $('.'+selector).closest("article").addClass('order-agency');
+    }
+})(jQuery);
+
+
+
+  
+  
+  
