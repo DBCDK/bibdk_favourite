3b06ec3ab3a25dd3976519a8e8ebfddbcbc1de3d
pjohans <pjohans@gmail.com>
2013-05-29 13:19:19 +0200

    getUserStatusRequest on favourite object

--- FavouriteAgency.php
+++ FavouriteAgency.php
@@ -21,7 +21,7 @@ class FavouriteAgency extends TingAgency {
   public function getUserData() {
     return $this->userData;
   }
-  
+
   /** Get userid for favourite
    * 
    * There are a number of possible userids. Run through userdata and return
@@ -29,27 +29,49 @@ class FavouriteAgency extends TingAgency {
    * 
    * @return boolean 
    */
-   public function getUserId() {
-     $possibilities = array('cpr','userId','barcode','cardno','customId',);
-     foreach ($possibilities as $pos) {
-       if (isset($this->userData[$pos])) {
-         return $this->userData[$pos];
-       }
-     }
-     return FALSE;
-   }
-   
-   public function getPinCode () {
-     return isset($this->userData['pincode']) ? $this->userData['pincode'] : FALSE;
-   }
-   
-   public function getUserStatus() { 
-     return isset($_SESSION['userStatus'][$this->getAgencyId()]) ? $_SESSION['userStatus'][$this->getAgencyId()] : FALSE;
-   }
-   
-   public function setUserStatus($res){
-     $_SESSION['userStatus'][$this->getAgencyId()] = $res;
-   }
+  public function getUserId() {
+    $possibilities = array('cpr', 'userId', 'barcode', 'cardno', 'customId',);
+    foreach ($possibilities as $pos) {
+      if (isset($this->userData[$pos])) {
+        return $this->userData[$pos];
+      }
+    }
+    return FALSE;
+  }
+
+  public function getPinCode() {
+    return isset($this->userData['pincode']) ? $this->userData['pincode'] : FALSE;
+  }
+
+  public function getUserStatus() {
+    // check if userstatus is already in $_SESSION
+    if (isset($_SESSION['userStatus'][$this->getAgencyId()])) {
+      return $_SESSION['userStatus'][$this->getAgencyId()];
+    }
+    // get parameters
+    $userId = $this->getUserId();
+    $userPincode = $this->getPinCode();
+    $libraryCode = $this->getAgencyId();
+
+    if (module_exists('ting_openuserstatus')) {
+      // get userstatus from webservice
+      $response = ting_openuserstatus_do_userstatus($userId, $userPincode, $libraryCode);
+      if (isset($response['error'])) {
+        return $response;
+      }
+      // no errors found. add response to $_SESSION
+      $_SESSION['userStatus'][$this->getAgencyId()] = $response;
+      // last check
+      return isset($_SESSION['userStatus'][$this->getAgencyId()]) ? $_SESSION['userStatus'][$this->getAgencyId()] : FALSE;
+    }
+    // cannot retrive userstatus
+    return FALSE;
+  }
+
+  public function setUserStatus($res) {
+    $_SESSION['userStatus'][$this->getAgencyId()] = $res;
+  }
+
 }
 
 ?>
